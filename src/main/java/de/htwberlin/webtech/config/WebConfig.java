cf8d5f6c5f177d0e719f0c8c5bd9111039545ba3
SebDrasdo <sepp.sama4gameplay@gmail.com>
2021-12-17 22:35:15 +0100

    fixed WebConfig

--- src/main/java/de/htwberlin/webtech/config/WebConfig.java
+++ src/main/java/de/htwberlin/webtech/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("api/**")
+        registry.addMapping("/api/**")
                 .allowedMethods("*")
                 .allowedOrigins(
                         "https://localhost:3000",
